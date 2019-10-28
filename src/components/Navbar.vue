a5d7c91331adf29b72bc72f9049f7b03e93333c3
Basitkhan23 <basitk749@gmail.com>
2019-10-29 01:59:12 +0500

    Adding New Components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,133 @@
 <template>
-    <div>
-        <b-navbar toggleable="lg" type="dark">
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+  <v-card>
+    <v-toolbar
+      color="deep-purple accent-4"
+      dark
+      flat
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <v-toolbar-title>Page title</v-toolbar-title>
 
-            <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
+      <v-spacer></v-spacer>
 
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-                <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                </b-nav-form>
-            </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs
+          v-model="currentItem"
+          background-color="transparent"
+          fixed-tabs
+          slider-color="white"
+        >
+          <v-tab
+            v-for="item in items"
+            :key="item"
+            :href="'#tab-' + item"
+          >
+            {{ item }}
+          </v-tab>
+
+          <v-menu
+            v-if="more.length"
+            bottom
+            left
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                text
+                class="align-self-center mr-4"
+                v-on="on"
+              >
+                more
+                <v-icon right>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+
+            <v-list class="grey lighten-3">
+              <v-list-item
+                v-for="item in more"
+                :key="item"
+                @click="addItem(item)"
+              >
+                {{ item }}
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="currentItem">
+      <v-tab-item
+        v-for="(item, index) in items.concat(more)"
+        :key="(item, index)"
+        :value="'tab-' + item" 
+      >
+        <v-card flat>
+          <v-card-text>
+            <h2>{{item}}</h2>
+            <!-- inserting new components from contents-->
+             <div v-if="item == 'Web'">
+                 <h1>{{texts[0]}}</h1>
+                 <h2>Web details</h2>
+                <!-- <Map /> -->
+            </div>
+            <div v-if="item == 'Shopping'">
+                 <h1>Shopping section</h1>
+                <!-- <Map /> -->
+            </div>
+            <div v-else-if="item == 'Videos'">
+                <h1>Videos section</h1>
+            </div>
+            <div v-else-if="item == 'Images'">
+                <h1>Images section</h1>
+            </div>
+            <div v-else-if="item == 'Maps'">
+                <Map />
+            </div>
+            <div v-else-if="item == 'Apps'">
+                <h1>Apps section</h1>
+            </div>
+            
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
 </template>
 
-
 <script>
-export default {
-    
-}
+  import Map from './contents/Map'
+  export default {
+    components: {Map},  
+    data: () => ({ 
+      currentItem: 'tab-Web',
+      items: [
+        'Web', 'Shopping', 'Videos', 'Images',
+      ],
+      more: [
+        'Maps', 'Apps',
+      ],
+      texts: 
+      [' w','s','v','i',],
+    }),
+
+    methods: {
+      addItem (item) {
+        const removed = this.items.splice(0, 1) 
+        this.items.push(
+          ...this.more.splice(this.more.indexOf(item), 1)
+        )
+        this.more.push(...removed)
+        this.$nextTick(() => { this.currentItem = 'tab-' + item })
+      },
+    },
+  }
 </script>
-
-
-<style lang="stylus" scoped>
-
-</style>
---
+++ src/components/contents/Map.vue
@@ -0,0 +1,9 @@
+<template>
+    <H1>Hello there, this is map</H1>
+</template>
+
+<script>
+export default {
+    name:'Map'
+}
+</script>
